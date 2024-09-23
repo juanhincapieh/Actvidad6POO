4f979a7bad178b8800efd92b988dcb018f77f44b
Juan Hincapie <50083597+juanhincapieh@users.noreply.github.com>
2024-09-23 18:52:02 -0500

    Create Principal.java

---
+++ crudfuncionalidades/Principal.java
@@ -0,0 +1,11 @@
+package crudfuncionalidades;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+
+        CRUDVentana ventanaFuncionalidades;
+        ventanaFuncionalidades = new CRUDVentana();
+        ventanaFuncionalidades.setVisible(true);
+    }
+}
